19a12b8f832ac246fe34a7eb3d2329c44dcdaa69
litsher <litsher@gmail.com>
2016-02-23 17:14:12 +0100

    Removed delete header, didn't need it at this time

--- Navi/src/Navi.Backend/wwwroot/Scripts-ng/controllers.js
+++ Navi/src/Navi.Backend/wwwroot/Scripts-ng/controllers.js
@@ -1,9 +1,7 @@
 ﻿var navigatorApp = angular.module('navigatorApp', []);
 
 navigatorApp.service('dataService', function ($http) {
-delete $http.defaults.headers.common['X-Requested-With'];
 this.getData = function() {
-    // $http() returns a $promise that we can add handlers with .then()
     return $http({
         method: 'GET',
         url: 'http://localhost:59184/api/location',
