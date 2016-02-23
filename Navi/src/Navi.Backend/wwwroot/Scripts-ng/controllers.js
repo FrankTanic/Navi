3d38a18035018b2131fb93b922a76e7ce6f11159
litsher <litsher@gmail.com>
2016-02-23 15:30:52 +0100

    Added Angular Controller

---
+++ Navi/src/Navi.Backend/wwwroot/Scripts-ng/controllers.js
@@ -0,0 +1,21 @@
+﻿var navigatorApp = angular.module('navigatorApp', []);
+
+navigatorApp.controller('LocationListCtrl', function ($scope) {
+    $scope.locations = [
+      {
+          'name': 'BLiS',
+          'description': 'Where the cool kids roam.',
+          'coords': 'None jet.'
+      },
+      {
+          'name': 'Microsoft',
+          'description': 'You love them or you hate them.',
+          'coords': 'None jet.'
+      },
+      {
+          'name': 'Those people from Iphone and stuff',
+          'description': 'Heard the cult leader passed away recently.',
+          'coords': 'None jet.'
+      }
+    ];
+});
