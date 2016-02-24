ff2d4868dd0b7250050bcade73858b3bf01240b7
litsher <litsher@gmail.com>
2016-02-24 16:17:19 +0100

    Now added a draggable marker, which is the at the moment fictional User location, on placement of the marker, you will see the distance between the two markers.

--- Navi/src/Navi.Backend/wwwroot/Scripts/mapsLogic.js
+++ Navi/src/Navi.Backend/wwwroot/Scripts/mapsLogic.js
@@ -1,21 +1,31 @@
 ﻿var geocoder = new google.maps.Geocoder();
-var latLng = JSON.parse(localStorage.getItem("lastLocation"));
-if (latLng === null || latLng === 'undefined') {
-    latLng = new google.maps.LatLng(51.871784, 4.546146);
-}
-
+var latLngMob = new google.maps.LatLng(51.8719992870816, 4.5463230257950045);
+var latLng = new google.maps.LatLng(51.871784, 4.546146);
 function initialize() {
     var map = new google.maps.Map(document.getElementById('mapCanvas'), {
-        zoom: 20,
+        zoom: 18,
         center: latLng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     var marker = new google.maps.Marker({
-        position: latLng,
+        position: latLngMob,
         title: 'Point A',
         map: map,
         draggable: true
     });
+    var markerloc = new google.maps.Marker({
+        position: latLng,
+        title: 'Point B',
+        map: map,
+        draggable: false
+    });
+    // Add circle overlay and bind to marker
+    var circle = new google.maps.Circle({
+        map: map,
+        radius: 50,    
+        fillColor: '#AA0000'
+    });
+    circle.bindTo('center', markerloc, 'position');
 
     // Update current position info.
     updateMarkerPosition(latLng);
@@ -34,6 +44,7 @@ function initialize() {
     google.maps.event.addListener(marker, 'dragend', function () {
         updateMarkerStatus('Drag ended');
         geocodePosition(marker.getPosition());
+        radiusCheck(latLng, marker.getPosition())
     });
 }
 
@@ -66,4 +77,10 @@ function updateMarkerAddress(str) {
 function saveLocation() {
     latLng = document.getElementById('info').innerHTML;
     localStorage.setItem("lastLocation", JSON.stringify(latLng));
+}
+
+function radiusCheck(latLng, latLngMob)
+{
+    var distance = google.maps.geometry.spherical.computeDistanceBetween(latLng, latLngMob);
+    alert(distance);
 }
