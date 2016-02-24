6adb7a837ce1434dd32b8da9f40df6f0459c1bf5
litsher <litsher@gmail.com>
2016-02-24 11:23:30 +0100

    Added routing module

--- Navi/src/Navi.Backend/wwwroot/Scripts-ng/app.js
+++ Navi/src/Navi.Backend/wwwroot/Scripts-ng/app.js
@@ -1,4 +1,17 @@
 ﻿var navigatorApp = angular.module('navigatorApp', [
   'ngRoute',
-  'phonecatControllers'
+  'navigatorControllers'
 ]);
+
+
+navigatorApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/locations', {
+            templateUrl: 'Partials/location-list.html',
+            controller: 'LocationListCtrl'
+        }).
+        otherwise({
+            redirectTo: '/locations'
+        });
+  }]);
