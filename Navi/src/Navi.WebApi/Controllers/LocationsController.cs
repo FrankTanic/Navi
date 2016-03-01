be01d2dd2b716fa75804b0f7932638059be76d15
Frank van Driel <frank.vandriel@hotmail.com>
2016-03-01 15:11:03 +0100

    Updated location code

---
+++ Navi/src/Navi.WebApi/Controllers/LocationsController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using Navi.WebApi.Models;
+
+namespace Navi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocationsController : Controller
+    {
+        private readonly NaviDbContext _db;
+
+        public LocationsController(NaviDbContext db)
+        {
+            _db = db;
+        }
+
+        // Get all locations
+        [HttpGet]
+        public IEnumerable<Location> Get()
+        {
+            return _db.Locations.ToList();
+        }
+
+        // GET location
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Location location = _db.Locations.SingleOrDefault(l => l.Id == id);
+
+            if (location == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            return new HttpOkObjectResult(location);
+        }
+
+        // POST location
+        [HttpPost]
+        public IActionResult Post([FromBody]Location location)
+        {
+            if (location == null)
+            {
+                return new BadRequestResult();
+            }
+
+            _db.Add(location);
+            _db.SaveChanges();
+
+            return new HttpOkObjectResult(location);
+        }
+
+        // PUT location
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Location location)
+        {
+            if (location == null)
+            {
+                return new BadRequestResult();
+            }
+
+            _db.Update(location);
+            _db.SaveChanges();
+
+            return new HttpOkResult();
+        }
+
+        // DELETE location
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Location location = _db.Locations.SingleOrDefault(l => l.Id == id);
+
+            if (location == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            _db.Remove(location);
+            _db.SaveChanges();
+
+            return new HttpOkResult();
+        }
+    }
+}
